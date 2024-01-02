9755aa08473e8440d9925b12a6b689e62bf57429
Smarika Tripathi <134390967+Smarikatripathi@users.noreply.github.com>
2024-01-02 07:54:07 +0545

    Add files via upload

---
+++ DDA.cpp
@@ -0,0 +1,35 @@
+#include<graphics.h>
+#include<stdio.h>
+#include<math.h>
+#include<dos.h>
+int main()
+{
+	float x,y,x1,x2,y1,y2,dx,dy,step;
+	int i,gd=DETECT,gm;
+	initgraph(&gd,&gm,(char*)"");
+	printf("enter the value of x1 and y1:");
+	scanf("%f%f",&x1,&y1);
+	printf("enter the value of x2 and y2:");
+	scanf("%f%f",&x2,&y2);
+	dx=abs(x2-x1);
+	dy=abs(y2-y1);
+	if(dx>=dy)
+	step=dx;
+	else
+	step=dy;
+	dx=dx/step;
+	dy=dy/step;
+	x=x1;
+	y=y1;
+	i=1;
+	while(i<=step)
+	{
+		putpixel(x,y,WHITE);
+		x=x+dx;
+		y=y+dy;
+		i=i+1;
+		delay(100);
+		}
+		closegraph();
+}
+
