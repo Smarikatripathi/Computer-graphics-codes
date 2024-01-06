97f015f623ad9f41dee75fd14ed58b1d4c38deec
Smarika Tripathi <134390967+Smarikatripathi@users.noreply.github.com>
2024-01-06 18:48:19 +0545

    Update circle.cpp

--- circle.cpp
+++ circle.cpp
@@ -1,22 +1,47 @@
 
-#include<stdio.h>
-#include<conio.h>
-#include<graphics.h>
-void draw_circle(int,int,int);
-void symmetry(int,int,int);
-int main()
+
+#include <conio.h> 
+#include <dos.h> 
+#include <graphics.h> 
+#include <stdio.h> 
+
+// Driver code 
+int main() 
+{ 
+int x,y,x_mid , y_mid , radius,dp;
+int g_mode ,g_driver= DETECT;
+//clrscr();
+initgraph(&g_driver,&g_mode," ");
+printf("************ MID POINT CIRCLE DRAWING ALOGORITHM ******\n\n");
+printf("\n enter the coordinates=");
+scanf("%d %d",&x_mid,&y_mid);
+printf("\n now enter the radius =");
+scanf("%d",&radius);
+x=0;
+y= radius;
+dp=1-radius;
+do
 {
-	int xc,yc,R;
-	int gd=DETECT,gm;
-	initgraph(&gd,&gm,"(char*)");
-	printf("Enter the center of the circle:\n");
-	printf("xc=");
-	scanf("%d",&xc);
-	printf("yc=");
-	scanf("%d",&yc);
-	printf("Enter the radius of the circle");
-	scanf("%d",&R);
-	draw_circle(xc,yc,R);
-	getch();
-	closegraph();
+	putpixel (x_mid+x,y_mid+y, YELLOW);
+	putpixel (x_mid+y,y_mid+x, YELLOW);
+	putpixel (x_mid-y,y_mid+x, YELLOW);
+	putpixel (x_mid-x,y_mid+y, YELLOW);
+	putpixel (x_mid-x,y_mid-y, YELLOW);
+	putpixel (x_mid-y,y_mid-x, YELLOW);
+	putpixel (x_mid+y,y_mid-x, YELLOW);
+	putpixel (x_mid+x,y_mid-y, YELLOW);
+	if(dp<0){
+		dp+=(2*x)+1;
+		
+	}
+	else {
+		y=y-1;
+		dp+=(2*x)-(2*y)+1;
+		
+	}
+	x=x+1;
+	
+}while (y>x);
+getch();
+return 0;
 }
